c22de18fa265af2e2482e9f14d9b828d7578daf3
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-23 17:46:08 +0200

    A function that frees stack

---
+++ free_stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * free_stack - Frees a stack
+ * @stack: A pointer to the top of the stack
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
+
+	while (stack != NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+}
