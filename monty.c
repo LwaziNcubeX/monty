efb7f11abae18e4fb43d401aecfe760b096fc1b9
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-23 17:44:01 +0200

    The monty program

---
+++ monty.c
@@ -0,0 +1,64 @@
+#include "monty.h"
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+stack_t *stack = NULL;
+unsigned int line_number = 0;
+
+int main(int argc, char *argv[])
+{
+	char *line_buf = NULL, *opcode;
+	size_t line_buf_size = 0;
+	int i = 0;
+	FILE *file;
+
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	check_argc(argc);
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&line_buf, &line_buf_size, file) != -1)
+	{
+		line_number++;
+		opcode = strtok(line_buf, " $\n");
+		if (opcode == NULL || opcode[0] == '#')
+			continue;
+
+		while (instructions[i].opcode != NULL)
+		{
+			if (strcmp(opcode, instructions[i].opcode) == 0)
+			{
+				instructions[i].f(&stack, line_number);
+				break;
+			}
+			i++;
+		}
+		if (instructions[i].opcode == NULL)
+		{
+			fprintf(stderr, "L%i: unknown instruction %s\n", line_number, opcode);
+			free(line_buf);
+			free_stack(stack);
+			fclose(file);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	free(line_buf);
+	free_stack(stack);
+	fclose(file);
+	exit(EXIT_SUCCESS);
+}
