ebeb22051de1c80c5a3f40819127d44f47812c8c
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-25 01:31:24 +0200

    Added function to get opcodes.

---
+++ get_instructions.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * get_instructions - Returns an array of instruction_t structs
+ *
+ * Return: An array of instruction_t structs
+ */
+instruction_t *get_instructions(void)
+{
+	static instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{NULL, NULL}
+	};
+
+	return (instructions);
+}
