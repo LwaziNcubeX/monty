975785cbf856fa5ea17ad1f2377ddbe0ee14ace8
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-25 00:52:09 +0200

    refactored code and made some improvements

--- push.c
+++ push.c
@@ -6,32 +6,31 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	char *num_str = strtok(NULL, " \t\n\r");
-	int num;
+	char *arg = strtok(NULL, " \t\n\r");
+	int n;
 	stack_t *new_node;
 
-	if (num_str == NULL || (!isdigit(*num_str) && *num_str != '-'))
+	if (arg == NULL || !is_number(arg))
 	{
 		fprintf(stderr, "L%i: usage: push integer\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	num = atoi(num_str);
+
+	n = atoi(arg);
 	new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = num;
+	new_node->n = n;
+	new_node->next = (*stack);
 	new_node->prev = NULL;
 
-	if (*stack == NULL)
-		new_node->next = NULL;
-	else
-	{
-		new_node->next = *stack;
+	if ((*stack) != NULL)
 		(*stack)->prev = new_node;
-	}
-	*stack = new_node;
+	(*stack) = new_node;
 }
