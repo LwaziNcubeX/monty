d98e2145a31c1696cbeaaf737c7e0ae57c48ef6a
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-25 00:47:36 +0200

    Added function to interpret input file

---
+++ interpreter.c
@@ -0,0 +1,59 @@
+#include "monty.h"
+
+/**
+ * interpret_file - interpret the given file
+ *
+ * @file: pointer to file to interpret
+ */
+void interpret_file(FILE *file)
+{
+	char *line_buf = NULL;
+	size_t line_buf_size = 0;
+	unsigned int line_number = 0;
+
+	while (getline(&line_buf, &line_buf_size, file) != -1)
+	{
+		line_number++;
+		execute_instruction(line_buf, line_number);
+	}
+
+	free(line_buf);
+}
+
+/**
+ * execute_instruction - execute the given instruction
+ *
+ * @line_buf: buffer containing the instruction
+ * @line_number: line number of the instruction
+ */
+void execute_instruction(char *line_buf, unsigned int line_number)
+{
+	char *opcode;
+	int i = 0;
+
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{NULL, NULL}
+	};
+
+	opcode = strtok(line_buf, " \t\n\r");
+	if (opcode == NULL || opcode[0] == '#')
+		return;
+
+	while (instructions[i].opcode != NULL)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(&stack, line_number);
+			return;
+		}
+		i++;
+	}
+
+	fprintf(stderr, "L%i: unknown instruction %s\n", line_number, opcode);
+	free_stack(stack);
+	exit(EXIT_FAILURE);
+}
